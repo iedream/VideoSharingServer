0a4dc1c09b1a1e4a4238afa13391f41451564d1f
Catherine Zhao <iedream@Catherines-MacBook-Pro.local>
2016-04-02 18:28:58 -0400

    add mongoose connection

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connection(MONGOLAB_URI || 'mongodb://localhost/app');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
