278def5d9f5f80c7008cbe890e8f260e7f8ebba5
Catherine Zhao <iedream@Catherines-MacBook-Pro.local>
2016-04-04 21:58:49 -0400

    ios testing 50

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,147 @@ var async = require('async');
 
 var mongoose = require('mongoose');
 
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
+
 var dataSchema = new mongoose.Schema({
     title: {type: String},
-    data: {type: Object}
+    data: {type: Object},
+    groupId: {type: String}
 });
 var Data = mongoose.model('Data', dataSchema);
 
+var groupSchema = new mongoose.Schema({
+    groupId: {type: String},
+    password: {type: Object}
+});
+var Group = mongoose.model('Group', groupSchema);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.send({'message':'hello'});
 });
 
-router.post('/post_data', function(req, res, next) {
+router.post('/create/group', function(req, res, next) {
+    if (req.body.groupId) {
+        return res.send(404, {'error': 'missing group id'});
+    }
+    if (req.body.password) {
+        return res.send(404, {'error': 'missing password'});
+    }
+    var groupId = req.body.groupId;
+    var password = bcrypt.hashSync(req.body.password, salt);
+
+    async.waterfall([
+        function(cb) {
+            Data.find({groupId: groupId}, function(err, foundData) {
+                if(err) {
+                    return cb({message: err.message, code: 500});
+                }
+                if(foundData.length !== 0) {
+                    return cb({message: 'group already exist', code: 400})
+                }
+                cb();
+            })
+        },
+        function(cb) {
+            var group = new Group({
+                groupId: groupId,
+                password: password
+            });
+            group.save(function(err) {
+                if(err) {
+                    return cb({message: err.message, code: 500});
+                }
+                cb(null, {'data': 'group created successfully'})
+            })
+        }
+    ], function(err, success) {
+        if(err) {
+            return res.send(err.code, {'error':err.message});
+        }
+        res.send(200, success)
+    })
+});
+
+router.post('/group/post/data', function(req, res, next) {
     if(!req.body.name) {
-        return res.send(404, {'message':'missing name'});
+        return res.send(404, {'error':'missing name'});
     }
     if(!req.body.data) {
-        return res.send(404, {'message':'missing data'});
+        return res.send(404, {'error':'missing data'});
+    }
+    if(!req.body.groupId) {
+        return res.send(404, {'error':'missing group id'});
+    }
+    if(!req.body.password) {
+        return res.send(404, {'error':'missing password'});
+    }
+    var groupId = req.body.groupId;
+    var password = req.body.password;
+    var plistName = req.body.name;
+    var plistData = req.body.data;
+    var newPlaylist = false;
+
+    async.waterfall([
+        function(cb) {
+            Group.find({groupId:groupId}, function(err, group) {
+                if(err) {
+                    return cb({message: err.message, code: 500});
+                }
+                if(!bcrypt.compareSync(password, group[0].password)) {
+                    return cb({message: 'Wrong Password', code: 401});
+                }
+                cb();
+            })
+        },
+        function(cb) {
+            Data.find({title: plistName, groupId: groupId}, function(err, foundData) {
+                if(err) {
+                    return cb({message: err.message, code: 500});
+                }
+                if(foundData.length === 0) {
+                    newPlaylist = true;
+                    var data = new Data({
+                        title: plistName,
+                        data: plistData,
+                        groupId: groupId
+                    });
+                    return cb(null, data)
+                }
+                cb(null, foundData[0]);
+            })
+        },
+        function(data, cb) {
+            if(!newPlaylist) {
+                data.data = plistData;
+            }
+            data.save(function(err) {
+                if(err) {
+                    return cb({message: err.message, code: 500});
+                }
+                if(!newPlaylist) {
+                    cb(null, {'data': 'upload successful'})
+                }else{
+                    cb(null, {'data': 'new playlist upload successful'})
+                }
+            })
+        }
+    ], function(err, success) {
+        if(err) {
+            return res.send(err.code, {'error':err.message});
+        }
+        res.send(200, success)
+    });
+});
+
+
+router.post('/post/data', function(req, res, next) {
+    if(!req.body.name) {
+        return res.send(404, {'error':'missing name'});
+    }
+    if(!req.body.data) {
+        return res.send(404, {'error':'missing data'});
     }
     var plistData = req.body.data;
     var plistName = req.body.name;
@@ -29,7 +152,7 @@ router.post('/post_data', function(req, res, next) {
 
     async.waterfall([
         function(cb) {
-            Data.find({title: plistName}, function(err, foundData) {
+            Data.find({title: plistName, groupId: "public"}, function(err, foundData) {
                 if(err) {
                     return cb({'error': err.message});
                 }
@@ -37,7 +160,8 @@ router.post('/post_data', function(req, res, next) {
                     newPlaylist = true;
                     var data = new Data({
                         title: plistName,
-                        data: plistData
+                        data: plistData,
+                        groupId: "public"
                     });
                     return cb(null, data)
                 }
@@ -68,12 +192,58 @@ router.post('/post_data', function(req, res, next) {
     });
 });
 
-router.get('/get_data', function(req, res, next) {
+router.get('/group/get/data', function(req, res, next) {
     if (!req.query.name) {
-        return res.send(404, {'message':'missing name'});
+        return res.send(404, {'error':'missing name'});
+    }
+    if (!req.query.groupId) {
+        return res.send(404, {'error':'missing group id'});
+    }
+    if (!req.query.password) {
+        return res.send(404, {'error':'missing password'});
     }
     var plistName = req.query.name;
-    Data.find({title: plistName}, function(err, foundData) {
+    var groupId = req.query.groupId;
+    var password = req.query.password;
+
+    async.waterfall([
+        function(cb) {
+            Group.find({groupId:groupId}, function(err, group) {
+                if(err) {
+                    return cb({message: err.message, code: 500});
+                }
+                if(!bcrypt.compareSync(password, group[0].password)) {
+                    return cb({message: 'Wrong Password', code: 401});
+                }
+                cb();
+            })
+        },
+        function(cb) {
+            Data.find({title: plistName, groupId: groupId}, function(err, foundData) {
+                if(err) {
+                    return cb({message: err.message, code: 500});
+                }
+                if(foundData.length === 0) {
+                    var message = 'no data found for: ' + plistName +' in ' + groupId;
+                    return cb({message: err.message, code: 401});
+                }
+                cb(null, {'data': foundData});
+            })
+        }
+    ], function(err, data) {
+        if(err) {
+            return res.send(err.code, {'error':err.message});
+        }
+        res.send(200, data)
+    })
+});
+
+router.get('/get/data', function(req, res, next) {
+    if (!req.query.name) {
+        return res.send(404, {'error':'missing name'});
+    }
+    var plistName = req.query.name;
+    Data.find({title: plistName, groupId: "public"}, function(err, foundData) {
         if(err) {
             return res.send(500,{'error': err.message});
         }
